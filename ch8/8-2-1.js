110ebb0b004e56aa6000d029de223383ce0a4488
mochabbang <sungmin9036@gmail.com>
2022-09-12 17:50:24 +0900

    8장 함수옮기기,필드옮기기 공부-22.09.12

--- ch8/8-2-1.js
+++ ch8/8-2-1.js
@@ -1,34 +1,38 @@
 export class Customer {
-  #name;
-  #discountRate;
-  #contract;
-  constructor(name, discountRate) {
-    this.#name = name;
-    this.#discountRate = discountRate;
-    this.#contract = new CustomerContract(this.dateToday());
-  }
+    #name;
+    #contract;
+    constructor(name, discountRate) {
+        this.#name = name;
+        this.#contract = new CustomerContract(this.dateToday(), discountRate);
+    }
 
-  get discountRate() {
-    return this.#discountRate;
-  }
+    becomePreferred() {
+        this.#contract.discountRate += 0.03;
+        // 다른 코드들이 있음...
+    }
 
-  becomePreferred() {
-    this.#discountRate += 0.03;
-    // 다른 코드들이 있음...
-  }
+    applyDiscount(amount) {
+        return amount.subtract(amount.multiply(this.#contract.discountRate));
+    }
 
-  applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
-  }
-
-  dateToday() {
-    return new Date();
-  }
+    dateToday() {
+        return new Date();
+    }
 }
 
 class CustomerContract {
-  #startDate;
-  constructor(startDate) {
-    this.#startDate = startDate;
-  }
+    #startDate;
+    #discountRate;
+    constructor(startDate, discountRate) {
+        this.#startDate = startDate;
+        this.#discountRate = discountRate;
+    }
+
+    get discountRate() {
+        return this.#discountRate;
+    }
+
+    set discountRate(value) {
+        this.#discountRate = value;
+    }
 }
