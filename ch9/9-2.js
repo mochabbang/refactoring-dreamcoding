17737d453bcb307b42df9ada3309cacfb69a55f4
mochabbang <sungmin9036@gmail.com>
2022-09-18 17:09:49 +0900

    9.1~9.4까지 진행. 22.09.18  -9.4 강의 다시듣기

--- ch9/9-2.js
+++ ch9/9-2.js
@@ -1,22 +1,27 @@
 class Organization {
-  constructor(data) {
-    this._name = data.name;
-    this._country = data.country;
-  }
-  get name() {
-    return this._name;
-  }
-  set name(value) {
-    this._name = value;
-  }
-  get country() {
-    return this._country;
-  }
-  set country(value) {
-    this._country = value;
-  }
+    #title;
+    #country;
+
+    constructor(data) {
+        this.#title = data.title !== undefined ? data.title : data.name;
+        this.#country = data.country;
+    }
+    get title() {
+        return this.#title;
+    }
+    set title(value) {
+        this.#title = value;
+    }
+    get country() {
+        return this.#country;
+    }
+    set country(value) {
+        this.#country = value;
+    }
 }
 const organization = new Organization({
-  name: '드림코딩',
-  country: '대한민국',
+    title: '드림코딩',
+    country: '대한민국',
 });
+
+console.log(organization.title, organization.country);
--- ch9/9-4.js
+++ ch9/9-4.js
@@ -1,65 +1,58 @@
 class Person {
-  #name;
-  #telephoneNumber;
-  constructor(name, areaCode, number) {
-    this.#name = name;
-    this.#telephoneNumber = new TelephoneNumber(areaCode, number);
-  }
+    #name;
+    #telephoneNumber;
+    constructor(name, areaCode, number) {
+        this.#name = name;
+        this.#telephoneNumber = new TelephoneNumber(areaCode, number);
+    }
 
-  get name() {
-    return this.#name;
-  }
+    get name() {
+        return this.#name;
+    }
 
-  set name(arg) {
-    this.#name = arg;
-  }
+    set name(arg) {
+        this.#name = arg;
+    }
 
-  get telephoneNumber() {
-    return this.#telephoneNumber.toString;
-  }
+    get telephoneNumber() {
+        return this.#telephoneNumber.toString;
+    }
 
-  get officeAreaCode() {
-    return this.#telephoneNumber.areaCode;
-  }
+    get officeAreaCode() {
+        return this.#telephoneNumber.areaCode;
+    }
 
-  set officeAreaCode(value) {
-    this.#telephoneNumber.areaCode = value;
-  }
+    set officeAreaCode(value) {
+        this.#telephoneNumber = new TelephoneNumber(value, this.#telephoneNumber.number);
+    }
 
-  get officeNumber() {
-    return this.#telephoneNumber.number;
-  }
+    get officeNumber() {
+        return this.#telephoneNumber.number;
+    }
 
-  set officeNumber(value) {
-    this.#telephoneNumber.number = value;
-  }
+    set officeNumber(value) {
+        this.#telephoneNumber = new TelephoneNumber(this.#telephoneNumber.areaCode, value);
+    }
 }
 
 class TelephoneNumber {
-  #areaCode;
-  #number;
-  constructor(area, number) {
-    this.#areaCode = area;
-    this.#number = number;
-  }
+    #areaCode;
+    #number;
+    constructor(area, number) {
+        this.#areaCode = area;
+        this.#number = number;
+    }
 
-  get areaCode() {
-    return this.#areaCode;
-  }
-  set areaCode(arg) {
-    this.#areaCode = arg;
-  }
+    get areaCode() {
+        return this.#areaCode;
+    }
+    get number() {
+        return this.#number;
+    }
 
-  get number() {
-    return this.#number;
-  }
-  set number(arg) {
-    this.#number = arg;
-  }
-
-  get toString() {
-    return `(${this.#areaCode}) ${this.#number}`;
-  }
+    get toString() {
+        return `(${this.#areaCode}) ${this.#number}`;
+    }
 }
 
 const person = new Person('엘리', '010', '12345678');
