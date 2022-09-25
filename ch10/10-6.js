a9aa24eca5a221ad864205ae766d4fc7f666a185
mochabbang <sungmin9036@gamil.com>
2022-09-26 08:22:06 +0900

    10.6 Assert적용 10.7 제어플래그 탈출문으로 바꾸기 작업-22.09.26

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,18 @@
+import { strict as assert } from "node:assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
-    return this.discountRate ? number - this.discountRate * number : number;
+    assert(number >= 0);
+
+    if (!this.discountRate) {
+      return number;
+    } else {
+      return number - this.discountRate * number;
+    }
   }
 }
+
+new Customer().applyDiscount(1);
