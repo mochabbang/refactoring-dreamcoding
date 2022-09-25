eb76a3218f71e35a68ee373bdfb43cfb656d5019
mochabbang <sungmin9036@gamil.com>
2022-09-26 08:46:10 +0900

    11.1 질의함수와 변경함수로 변경하기-22.09.26

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,36 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
-    (total, each) => each.amount + total,
-    0
-  );
-  sendBill();
-  return result;
+/* 함수 하나당 하나의 일!! */
+function totalOutstanding() {
+  return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+
+function sendBill() {
+  // Bill...
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
-  for (const p of people) {
-    if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
-    }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
-    }
+  if (findMiscreant(people) !== "") {
+    setOffAlarms(alarm, miscreant);
   }
-  return '';
+}
+
+function findMiscreant(people) {
+  return people.filter((p) => {
+    return "Don,John".includes(p) ? p : "";
+  });
+
+  // for (const p of people) {
+  //   if (p === "Don") {
+  //     return "Don";
+  //   }
+  //   if (p === "John") {
+  //     return "John";
+  //   }
+  // }
+  // return "";
 }
 
 function setOffAlarms(alarm, p) {
-  alarm.setOff('Found Miscreant ' + p);
+  setOffAlarms(alarm, p);
 }
